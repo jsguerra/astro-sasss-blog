021cb8db38f06f307a61ed35663c1d4ccd87a775
jsguerra <jose@gcostudios.com>
2023-02-23 16:43:34 +0000

    updated config

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,16 @@ import { z, defineCollection } from "astro:content";
 
 // Create custom schema
 const blog = defineCollection({
-  schema: z.object({})
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    draft: z.boolean(),
+    author: z.enum(["Jose Guerra"]),
+  })
 });
 
 export const collections = { blog };
